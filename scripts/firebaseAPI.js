1ed7e08d4ae52d8cc95fec541c4ca68750757682
Brian Hsieh <brianh0702@gmail.com>
2021-10-26 22:25:41 -0700

    Added navbar and login authentication

--- scripts/firebaseAPI.js
+++ scripts/firebaseAPI.js
@@ -1,13 +1,13 @@
 // Your web app's Firebase configuration
-var  firebaseConfig = {
-    apiKey: ,
-    authDomain: ",
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId: 
-  };
-  
+var firebaseConfig = {
+  apiKey: "AIzaSyDwqKQ2zhG9gFHSeJ01cAFTlnpFhM0Z-BQ",
+  authDomain: "comp1800-demo.firebaseapp.com",
+  projectId: "comp1800-demo",
+  storageBucket: "comp1800-demo.appspot.com",
+  messagingSenderId: "627248052487",
+  appId: "1:627248052487:web:9e70f14e192c3f7a956da2",
+};
 
-  const app = firebase.initializeApp(firebaseConfig);
-  
+
+const app = firebase.initializeApp(firebaseConfig);
+
